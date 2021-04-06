314d5b7e424c0414bf50dd7f79e80090adb4f835
StefanoCeli <stefanopsn91@gmail.com>
2021-04-06 14:14:19 +0200

    Fine esercizio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var nome = prompt('Inserisci il tuo nome');
+var cognome = prompt('Inserisci il tuo cognome');
+var colore = prompt('Scrivi il tuo colore preferito');
+var risultato = nome + cognome + colore + '21';
+document.getElementById('pass').innerHTML = risultato
