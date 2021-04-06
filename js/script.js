9f7eb1d334cf98ff7ce08fedb8fffe7d70ef0004
StefanoCeli <stefanopsn91@gmail.com>
2021-04-06 15:14:12 +0200

    aggiunto frase

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@ var nome = prompt('Inserisci il tuo nome');
 var cognome = prompt('Inserisci il tuo cognome');
 var colore = prompt('Scrivi il tuo colore preferito');
 var risultato = nome + cognome + colore + '21';
-document.getElementById('pass').innerHTML = risultato
+document.getElementById('pass').innerHTML = "Questa è la tua password: " + risultato;
